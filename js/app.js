2a9c195ee4cf252bc79a98b72c3b21cf8ae35126
Eugene <yujie.wu@mishi.cn>
2016-02-17 11:45:15 +0800

    injector servers

--- js/app.js
+++ js/app.js
@@ -7,13 +7,14 @@ var ms = angular.module('ms', [
     'ms.directives',
     'ms.controllers'
 ]);
+
 ms.value('userName', 'Eugene');//全局变量
 /*ms.config(['$routeProvider', function($routeProvider) {
-    //TemplateURl相对于index.html路径
-    $routeProvider.when('/home', {templateUrl: 'views/home.html', controller: 'HomeCtrl'});
-    $routeProvider.when('/view', {templateUrl: 'views/view.html', controller: 'ViewCtrl'});
-    $routeProvider.otherwise({redirectTo: '/home'});
-}]);*/
+ //TemplateURl相对于index.html路径
+ $routeProvider.when('/home', {templateUrl: 'views/home.html', controller: 'HomeCtrl'});
+ $routeProvider.when('/view', {templateUrl: 'views/view.html', controller: 'ViewCtrl'});
+ $routeProvider.otherwise({redirectTo: '/home'});
+ }]);*/
 
 ms.config(function ($controllerProvider, $compileProvider, $filterProvider, $provide) {
     ms.register = {
@@ -24,6 +25,18 @@ ms.config(function ($controllerProvider, $compileProvider, $filterProvider, $pro
         service: $provide.service
     }
 });
+
+ms.constant('app', {}).run(['app', '$injector', '$rootScope', function (app, $injector, $rootScope) {
+    angular.extend(app, {
+        $rootScope: $rootScope,
+        $timeout: $injector.get('$timeout'),
+        post: $injector.get('http').post,
+        get: $injector.get('http').get,
+        msApi: $injector.get('msApi'),
+        goToAnchor: $injector.get('goToAnchor')
+    })
+}]);
+
 ms.config(function ($routeProvider, $locationProvider) {
     var pageNames = [
         'login',        //登录
@@ -34,22 +47,22 @@ ms.config(function ($routeProvider, $locationProvider) {
         $routeProvider.when('/' + pageName, {
             templateUrl: 'view/' + pageName + '.html',
             controller: upFirstCharacter(pageName) + 'Ctrl',
-           /* resolve: {
-                load: function ($q, $route, $rootScope) {
-                    var deferred = $q.defer();
-                    var dependencies = [
-                        //'./js/controllers/home.js'
-                        './js/controllers/' + pageName + '.js'
-                    ];
-                    //动态加载js
-                    $script(dependencies, function () {
-                        $rootScope.$apply(function () {
-                            deferred.resolve();
-                        });
-                    });
-                    return deferred.promise;
-                }
-            }*/
+            /* resolve: {
+             load: function ($q, $route, $rootScope) {
+             var deferred = $q.defer();
+             var dependencies = [
+             //'./js/controllers/home.js'
+             './js/controllers/' + pageName + '.js'
+             ];
+             //动态加载js
+             $script(dependencies, function () {
+             $rootScope.$apply(function () {
+             deferred.resolve();
+             });
+             });
+             return deferred.promise;
+             }
+             }*/
             resolve: ['$q','$rootScope', function($q, $rootScope) {
                 var deferred = $q.defer();
                 var dependencies = [
@@ -65,7 +78,7 @@ ms.config(function ($routeProvider, $locationProvider) {
             }]
         });
     });
-    $routeProvider.otherwise({redirectTo: '/login'});
+    $routeProvider.otherwise({redirectTo: '/home'});
 });
 function upFirstCharacter(pageName) {
     var firstChar = pageName.slice(0, 1).toLocaleUpperCase();
--- js/services.js
+++ js/services.js
@@ -4,4 +4,87 @@
 'use strict';
 
 var msServices = angular.module('ms.services', []);
-msServices.value('version', '0.1');
+
+msServices.factory('msApi', ['$injector', function($injector){
+    var $interpolate = $injector.get('$interpolate');
+    var apiStore = {
+
+    };
+    return function msApi(name, id) {
+        var api = apiStore[name];
+        if (api) {
+            var exp = $interpolate(api);
+            api = exp({id: id});
+        } else {
+            api = '';
+        }
+        return api;
+    }
+}]);
+
+/**
+ * 服务器请求
+ * */
+msServices.factory('http', ['$http', '$timeout', function ($http, $timeout) {
+    function httpFactory(type) {
+        return function (api, data) {
+            if (type == 'get') api = api + "?" + parseQuery(data);
+            return $http({method: type, url: api, data: data, timeout: 20000, responseType: "json"})
+                .error(function (res, errCode) {
+                    console.log(res, errCode)
+                }).success(function (data) {
+                    if (data.auth == 0)alert("您没有该操作的权限")
+                });
+        }
+    }
+    return {
+        httpFactory: httpFactory,
+        get: httpFactory('get'),
+        post: httpFactory('post')
+    };
+}]);
+/**
+ * 格式化查询条件
+ * */
+function parseQuery(query) {
+    var ret = [], q = {};
+    if (angular.isArray(query)) {
+        angular.forEach(query, function (val) {
+            angular.extend(q, val);
+        })
+    } else {
+        q = query;
+    }
+    angular.forEach(q, function (value, key) {
+        if (key != "silent" && key != "skip") {
+            this.push(key + '=' + encodeURIComponent(value));
+        }
+    }, ret);
+    return ret.join("&");
+}
+
+/**
+ * 滚动到锚点 [jsgen anchorScroll](http://git.oschina.net/zensh/jsgen/blob/master/static/src/js/services.js)
+ * */
+msServices.factory('goToAnchor', ['$location', function ($location) {
+    function toView(element, top, height) {
+        var winHeight = $(window).height();
+
+        element = $(element);
+        if (element.length == 0) {
+            return
+        }
+        height = height > 0 ? height : 0;
+        $('html, body').animate({
+            scrollTop: top ? (element.offset().top - height) : (element.offset().top + element.outerHeight() + height - winHeight)
+        }, {
+            duration: 200,
+            easing: 'linear'
+        });
+    }
+
+    return function goToAnchor(anchor) {
+        $location.hash(anchor);
+        toView($('#' + anchor), 'top', 0);
+    }
+}]);
